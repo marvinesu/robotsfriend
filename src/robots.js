543facadfe14bf2cf69d1c9f7983b6567e5ae64f
marvinesu <marvinesu@gmail.com>
2020-02-23 23:46:28 -0600

    update

---
+++ src/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Emilia Alely Vivas Fonseca',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Jazmina Fonseca Sotelo',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Marvin Solis Urbina',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Natalia Soza Sotelo',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Liam Solis Fonseca',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Zoe Ximena Delgado Fonseca',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+    },
+    {
+      id: 11,
+      name: 'Josue Emmanuel Gonzales Fonseca',
+      username: 'josue.gonzales',
+      email: 'josue@gmail.com'
+    },
+  ];
