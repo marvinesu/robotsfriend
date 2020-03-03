2fcb7ab06e93d474ae0ab30aa0889d969af48fa0
marvinesu <marvinesu@gmail.com>
2020-03-02 23:27:42 -0600

    Updates

---
+++ precache-manifest.77262cc307b983dc3393c77b19a4048c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a008d4d0f6dd1dcc31a8c04a3feb16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fe77b75cdfd0d3de17a7",
+    "url": "/static/css/main.58d6672e.chunk.css"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/static/js/2.d6fca685.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d6fca685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe77b75cdfd0d3de17a7",
+    "url": "/static/js/main.94a0083a.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
